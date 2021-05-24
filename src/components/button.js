bcd4dd5f9f5af8da83da7dc202b8ecf05c5b70d1
kevinmalaverr <kevinmalaverr@gmail.com>
2021-05-24 18:12:41 -0500

    added button component

--- src/components/button.js
+++ src/components/button.js
@@ -1,9 +1,16 @@
-import '../styles/components/button.scss';
+import '../styles/components/button.scss'; // import styles in each component
 
+/**
+ * A component should return an string with the html
+ * it can receive props, recomend to pass it in an object and using destructure in the params
+ *
+ * @param {{name:string}} props
+ * @returns {string}
+ */
 function Button({ name }) {
   return `
-    <button>${name}</button>
+    <button class="btn">${name}</button>
   `;
 }
 
-export default Button;
+export default Button; // exports the function as default
