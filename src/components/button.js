2acf373d0d218607b8d3a1a0ce3662376a19f0d1
kevinmalaverr <kevinmalaverr@gmail.com>
2021-05-26 11:28:44 -0500

    added sb

--- src/components/button.js
+++ src/components/button.js
@@ -1,5 +1,17 @@
 import '../styles/components/button.scss'; // import styles in each component
 
+function html(str, ...labels) {
+  return str
+    .map((a, i) => {
+      if (typeof labels[i] === 'undefined') return a;
+      if (Array.isArray(labels[i])) return a + labels[i].join('');
+      if (typeof labels[i] === 'object')
+        throw new Error('cannot pass an object:');
+      return a + labels[i];
+    })
+    .join('');
+}
+
 /**
  * A component should return a string with the html
  *
@@ -7,9 +19,16 @@ import '../styles/components/button.scss'; // import styles in each component
  * @returns {string}
  */
 function Button({ name }) {
-  return `
-    <button class="btn">${name}</button>
+  return html`
+    <div class="g">
+      <div class="g">
+        <div class="aa"><a href="">${name}</a></div>
+        <p>${{ a: '4' }}</p>
+      </div>
+    </div>
   `;
 }
+
+var a = html` <div class="a"></div> `;
 
 export default Button; // exports the function as default
---
+++ src/stories/button.stories.js
@@ -0,0 +1,19 @@
+import Button from '../components/button';
+
+export default {
+  title: 'Components/Button',
+  component: Button,
+  argTypes: {
+    name: { control: 'text' },
+  },
+};
+
+export const Primary = Button.bind({}); // this line makes a copy of the "Button" function
+Primary.args = {
+  name: 'Primary button',
+};
+
+export const Buy = Button.bind({});
+Buy.args = {
+  name: 'Buy',
+};
