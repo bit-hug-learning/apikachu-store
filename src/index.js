1f185e52688837364b80c6e00bd3f9c4175ab26c
kevinmalaverr <kevinmalaverr@gmail.com>
2021-06-11 19:49:24 -0500

    added fetch pokemon range

--- src/index.js
+++ src/index.js
@@ -1,4 +1,12 @@
 import './styles/main.scss';
 import Footer from './components/Footer';
+import { getPokemonByRange } from './utils/fetchData';
 
 document.body.innerHTML = Footer({});
+
+async function a() {
+  const s = await getPokemonByRange(1, 4);
+  console.log(s);
+}
+
+a();
--- webpack.config.js
+++ webpack.config.js
@@ -17,16 +17,16 @@ module.exports = {
   },
   module: {
     rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env'],
-          },
-        },
-      },
+      // {
+      //   test: /\.js$/,
+      //   exclude: /node_modules/,
+      //   use: {
+      //     loader: 'babel-loader',
+      //     options: {
+      //       presets: ['@babel/preset-env'],
+      //     },
+      //   },
+      // },
       {
         test: /\.s[ac]ss$/i,
         use: [
