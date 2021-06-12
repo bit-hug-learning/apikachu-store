c1ccfe4897887118fe10c767adafcaf8094fc6c2
kevinmalaverr <kevinmalaverr@gmail.com>
2021-06-12 13:33:47 -0500

    finished hero

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -8,8 +8,11 @@ import starImg from '../assets/icons/star.svg';
  */
 function Hero() {
   return /*html*/ `
+    <div class="banner">
+      FREE Shipping with orders over $35!
+      <a href="" class="banner__link">Learn more<a>
+    </div>
     <div class="hero">
-      
         <div class="hero__image">
           <picture>
             <source srcset="${imageTablet}" media="(min-width: 764px)"/>
@@ -19,10 +22,10 @@ function Hero() {
         <div class="hero__text">       
           <div class="hero__title">
             <p>Let's Go<p>
-            <h1>Pikachu<h1>
+            <h1>Pikachu</h1>
           </div>
           <div class="hero__cta">
-            <span>$4.99</span><button class="btn btn--buy btn--big">Shop now</button> <img src="${starImg}"/>
+            <span class="hero__price">$4.99</span><button class="btn btn--buy btn--big">Shop now</button> <img src="${starImg}"/>
           </div>
         </div>
       
