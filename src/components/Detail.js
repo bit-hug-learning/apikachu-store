e3243893fd32bee383ff0da5b5b4ed901f0c6051
unknown <gvidal.industrial@gmail.com>
2021-06-20 15:27:41 -0500

    Added pokedex in mobile, tablet and desktop layout

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -3,8 +3,10 @@ import pokedexTop from '../assets/images/pokedex_top.svg';
 import pokedexBottom from '../assets/images/pokedex_bottom.svg';
 import pokedexRight from '../assets/images/pokedex_right.svg';
 import pokedexHinge from '../assets/images/pokedex_hinge.svg';
+import pokedexHingeV from '../assets/images/pokedex_hinge-v.svg';
 import leftArrow from '../assets/icons/leftArrow.svg';
 import pokemonTypes from '../pokemonTypes';
+
 
 function Stats(stats) {
   return stats
@@ -15,64 +17,77 @@ function Stats(stats) {
 function Detail({ image, id, name, types = [], stats, weight, height } = {}) {
   return /*html*/ `
     <div class="pokedex">
-      <img class="pokedex__top" src=${pokedexTop} />
-      <div class="pokedex__content-1">
-        <div class="pokedex__frame-container">
-          <div class="pokedex__image">
-            <img src="${image}" alt="" />
-          </div>
-        </div>
-        
-        <div class="pokedex__frame-container name">
-          <div class="pokedex__name">
-            <h2>#${id} ${name}</h2>
-          </div>
-        </div>
+      
+      <div class="pokedex__content-left">
+        <div class="pokedex__container-top">
+          <img class="pokedex__top" src=${pokedexTop} />
+          <div class="pokedex__container-hinge-v">
 
-        <div class="pokedex__buttons">
-          <button>
-            <img src="${leftArrow}" alt="" />
-          </button>
-          <div class="pokedex__frame-container">
-            <div class="pokedex__price">$${weight / 10}</div>
-          </div>
-          <button>
-            <img src="${leftArrow}" alt="" />
-          </button>
-        </div>
-      </div>
-      <img class="pokedex__hinge" src=${pokedexHinge} />
-      <div class="pokedex__content-2">
-        <img class="pokedex__right" src=${pokedexRight} />
-        <div class="pokedex__frame-container type">
-          <div class="pokedex__types">
-            Type
-            <img
-              src="${pokemonTypes[types[0]].image}"
-              alt="${pokemonTypes[types[0]].name}"
-            />
-            ${
-              types[1]
-                ? `<img src="${pokemonTypes[types[1]].image}" alt="${
-                    pokemonTypes[types[1]].name
-                  }" />`
-                : ''
-            }
-          </div>
-        </div>
-
-        <div class="pokedex__frame-container stat">
-          <div class="pokedex__stats">
-            <p>Stats</p>
-            <div class="pokedex__stats-data">
-              <div class="pokedex__stats-left">${Stats(stats)}</div>
-              <div class="pokedex__stats-right"><div>Weight: ${weight}</div> <div>Height: ${height}</div></div>
+            <img class="pokedex__hinge-v" src=${pokedexHingeV} />
+            <div class="pokedex__bottom-no-mobile">
             </div>
           </div>
         </div>
-  
+        <div class="pokedex__content-1">
+          <div class="pokedex__frame-container">
+            <div class="pokedex__image">
+              <img src="${image}" alt="" />
+            </div>
+          </div>
+          
+          <div class="pokedex__frame-container name">
+            <div class="pokedex__name">
+              <h2># ${id} ${name}</h2>
+            </div>
+          </div>
+
+          <div class="pokedex__buttons">
+            <button>
+              <img src="${leftArrow}" alt="" />
+            </button>
+            <div class="pokedex__frame-container">
+              <div class="pokedex__price">$ ${weight / 100}</div>
+            </div>
+            <button>
+              <img src="${leftArrow}" alt="" />
+            </button>
+          </div>
+        </div>
+        <img class="pokedex__hinge" src=${pokedexHinge} />
       </div>
-      <img class="pokedex__bottom" src=${pokedexBottom} />
+      <div class="pokedex__content-right">
+       <img class="pokedex__right" src=${pokedexRight} />
+        <div class="pokedex__content-2">
+          <div class="pokedex__frame-container type">
+            <div class="pokedex__types">
+              Type
+              <img
+                src="${pokemonTypes[types[0]].image}"
+                alt="${pokemonTypes[types[0]].name}"
+              />
+              ${
+                types[1]
+                  ? `<img src="${pokemonTypes[types[1]].image}" alt="${
+                      pokemonTypes[types[1]].name
+                    }" />`
+                  : ''
+              }
+            </div>
+          </div>
+
+          <div class="pokedex__frame-container stat">
+            <div class="pokedex__stats">
+              Stats
+              <div class="pokedex__stats-data">
+                <div class="pokedex__stats-left">${Stats(stats)}</div>
+                <div class="pokedex__stats-right"><div>Weight: ${weight / 10} Kg</div> <div>Height: ${height / 10} m</div></div>
+              </div>
+            </div>
+          </div>
+    
+        </div>
+        <img class="pokedex__bottom" src=${pokedexBottom} />
+      </div>        
     </div>
   `;
 }
