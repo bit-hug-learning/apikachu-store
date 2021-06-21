aee050d66517fb91822aefd70edcd765068992c0
Jose Manuel Rosales <josemanuelrosales2@gmail.com>
2021-06-21 12:24:24 -0500

    fixing some bugs

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,57 +1,71 @@
 import '../styles/components/filter.scss';
-
+import FilterIcon from '../assets/icons/filter.png'
 function Filter(){
   return `
-    <form class="filter">
-      <fieldset class="filter__price">
-        <legend>Price</legend>
-        <!--To know more about the dual slider go to: https://github.com/yairEO/ui-range-->
-        <div class="range-slider flat" 
-                    data-ticks-position='top' 
-                    style='--min:10; --max:1000; --prefix:"$"; --value-a:200; --value-b:800; --text-value-a:"200"; --text-value-b:"800";--thumb-size:15px; --thumb-color: #1492E6; --primary-color:#1492E6;'
-        >
-          <input id='range-min' type="range" min="10" max="1000" value="200" oninput="this.parentNode.style.setProperty('--value-a',this.value); this.parentNode.style.setProperty('--text-value-a', JSON.stringify(this.value))">
-          <output></output>
-          <input id='range-max' type="range" min="10" max="1000" value="800" oninput="this.parentNode.style.setProperty('--value-b',this.value); this.parentNode.style.setProperty('--text-value-b', JSON.stringify(this.value))">
-          <output></output>
-          <div class='range-slider__progress'></div>
-        </div>
+    <div class='filter'>
+      <div class='filter__icon'>
+        <img src="${FilterIcon}" alt="Filter"/>
+      </div>
+      <form class="form" id='form-type'>
+        <fieldset class="form__price">
+          <legend>Price</legend>
+          <!--To know more about the dual slider go to: https://github.com/yairEO/ui-range-->
+          <div class="range-slider flat" 
+                      data-ticks-position='top' 
+                      style='--min:10; --max:1000; --prefix:"$"; --value-a:200; --value-b:800; --text-value-a:"200"; --text-value-b:"800";--thumb-size:15px; --thumb-color: #1492E6; --primary-color:#1492E6;'
+          >
+            <input id='range-min' type="range" min="10" max="1000" value="200" oninput="this.parentNode.style.setProperty('--value-a',this.value); this.parentNode.style.setProperty('--text-value-a', JSON.stringify(this.value))">
+            <output></output>
+            <input id='range-max' type="range" min="10" max="1000" value="800" oninput="this.parentNode.style.setProperty('--value-b',this.value); this.parentNode.style.setProperty('--text-value-b', JSON.stringify(this.value))">
+            <output></output>
+            <div class='range-slider__progress'></div>
+          </div>
 
-        <div class='filter__price-numbers'>
-          <input id='minprice' type='number' min='10' max='1000' placeholder='$ - min'>
-          <hr/>
-          <input id='maxprice' type='number' min='10' max='1000' placeholder='$ - max'>
-        </div>
-      </fieldset>
+          <div class='form__price-input'>
+            <input id='minprice' type='number' min='10' max='1000' placeholder='$ - min'>
+            <hr/>
+            <input id='maxprice' type='number' min='10' max='1000' placeholder='$ - max'>
+          </div>
+        </fieldset>
 
-      <fieldset class="filter__types">
-        <legend>Types</legend>
-        <label><input type="checkbox" value='normal'/> Normal</label>
-        <label><input type="checkbox" value='fire'/> Fire</label>
-        <label><input type="checkbox" value='electric'/> Electric</label>
-        <label><input type="checkbox" value='water'/> Water</label>
-        <label><input type="checkbox" value='steel'/> Steel </label>
-        <label><input type="checkbox" value='grass'/> Grass</label>
-        <label><input type="checkbox" value='dragon'/> Dragon</label>
-      </fieldset>
-    </form>
+        <fieldset class="form__types">
+          <legend>Types</legend>
+          <label><input type="checkbox" value='normal'/> Normal</label>
+          <label><input type="checkbox" value='fire'/> Fire</label>
+          <label><input type="checkbox" value='electric'/> Electric</label>
+          <label><input type="checkbox" value='water'/> Water</label>
+          <label><input type="checkbox" value='steel'/> Steel </label>
+          <label><input type="checkbox" value='grass'/> Grass</label>
+          <label><input type="checkbox" value='dragon'/> Dragon</label>
+        </fieldset>
+      </form>
+    </div>
   `
 }
 
 export default Filter
 
-window.addEventListener( 'load', () => {
+document.addEventListener( 'DOMContentLoaded', () => {
   const rangeSlider = document.querySelector(".range-slider")
+  const filterIcon = document.querySelector('.filter__icon')
+
+  const form = document.querySelector('#form-type')
+
   const minPrice = document.getElementById('minprice')
   const maxPrice = document.getElementById('maxprice')
 
   const minRange = document.getElementById('range-min')
   const maxRange = document.getElementById('range-max')
 
+  filterIcon.addEventListener('click', () => {
+    form.classList.toggle('form-visible')
+  })
+
   minRange.addEventListener('change', () => {
     minPrice.value = minRange.value
 
   })
+  //get this value since its the 
   maxRange.addEventListener('change', () => {
     maxPrice.value = maxRange.value
   })
