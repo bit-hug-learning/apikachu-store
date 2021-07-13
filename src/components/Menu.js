077716c9f455bab16010097428fa01617ba612ec
Gonzo <gvidal.industrial@gmail.com>
2021-07-13 14:24:36 -0500

    Add a home item in  menu list

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -5,6 +5,7 @@ import WishIcon from '../assets/icons/wish_icon.svg';
 import BagIcon from '../assets/icons/bag_icon.svg';
 import VectorIcon from '../assets/icons/Vector.svg';
 import HamburgerIcon from '../assets/icons/hamburger.svg';
+import router from '../router';
 
 /**
  * @param {{number:int}} props
@@ -24,6 +25,11 @@ function Menu({ number }) {
           </a>
         </div>
         <ul class="menu__list">
+          <li class="menu__item">
+            <a class="menu__link" href="${router.navigateTo("/")}">Home<span class="menu__icon item__home"
+            ><img src=${ApikachuLogo} alt="Apikachu logo" /></span
+            ></a>
+          </li>
           <li class="menu__item">
             <span class="menu__icon"
               ><img src=${UserIcon} alt="User icon" /></span
