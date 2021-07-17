4f3fd009ade71bea90ea0e66f6e9669d22843d25
Gonzo <gvidal.industrial@gmail.com>
2021-07-17 18:37:11 -0500

    Start ShoppingCart component

---
+++ src/components/ShoppingCart.js
@@ -0,0 +1,14 @@
+import Home from '../pages/Home';
+
+
+function ShoppingCart() {
+  return html`
+
+
+
+  `;
+}
+
+// const addToCartButton = document.querySelector("")
+
+export default ShoppingCart;
