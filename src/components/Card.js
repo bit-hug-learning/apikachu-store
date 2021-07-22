e0a7b40e2570bd2a0e76d794f81ad5ce172d6e3b
Gonzo <gvidal.industrial@gmail.com>
2021-07-21 23:29:25 -0500

    Add disabled feature to the add to cart button and alert

--- src/components/Card.js
+++ src/components/Card.js
@@ -92,17 +92,15 @@ const addToCartButton = () => {
 
   cardButton.forEach((element) => {
     function createAlert() {
-      console.log("clickeó");
       const alertBox = document.createElement("div");
       alertBox.textContent = "Added to Cart";
       element.appendChild(alertBox);
       alertBox.setAttribute("class", "card__added-to-cart")
       setTimeout(() => { element.removeChild(alertBox) }, 2000);
       element.removeEventListener("click", createAlert);
-      console.log(element.children[0])
+
       const buttonDisabled = document.createElement("div");
       buttonDisabled.innerHTML = Button('Already added to cart', 'btn btn--add', false, true);
-
       element.children[0].replaceWith(buttonDisabled);
     }
     element.addEventListener("click", createAlert);     
